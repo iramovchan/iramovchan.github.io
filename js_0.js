180a375cdbb60912ecd5d7e4bfbd9c9a9830ea70
iramovchan <irusya.movchan@gmail.com>
2025-03-19 11:22:55 +0100

    last changes

--- js_0.js
+++ js_0.js
@@ -19,16 +19,4 @@ document.addEventListener("DOMContentLoaded", () => {
         video.addEventListener("mouseenter", () => video.play());
         video.addEventListener("mouseleave", () => video.pause());
     });
-
-    // Side bubbles animation
-    const container = document.getElementById("side-bubbles-container");
-    for (let i = 0; i < 6; i++) {
-        const bubble = document.createElement("div");
-        bubble.classList.add("side-bubble");
-        bubble.style.width = `${Math.random() * 150 + 50}px`;
-        bubble.style.height = bubble.style.width;
-        bubble.style.top = `${Math.random() * 100}vh`;
-        bubble.style.left = Math.random() > 0.5 ? "-100px" : "calc(100vw + 50px)";
-        container.appendChild(bubble);
-    }
 });
